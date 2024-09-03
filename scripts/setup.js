1f17102e6b1a4a3d57095423c0d8fd36e6ab9ce4
Zyrenth <work@zyrenth.dev>
2024-09-03 18:53:59 +0200

    added git purge to setup script

--- scripts/setup.js
+++ scripts/setup.js
@@ -11,19 +11,36 @@ const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'packa
 
 console.log('[Setup]', 'Cleaning up...');
 
-fs.unlinkSync(path.join(__dirname, '..', 'README.md'));
-fs.unlinkSync(path.join(__dirname, '..', 'LICENSE'));
+try {
+    fs.unlinkSync(path.join(__dirname, '..', 'README.md'));
+    fs.unlinkSync(path.join(__dirname, '..', 'LICENSE'));
 
-fs.unlinkSync(path.join(__dirname, '..', './Banner.png'));
-fs.rmdirSync(path.join(__dirname, '..', './Badges'), { recursive: true });
+    fs.unlinkSync(path.join(__dirname, '..', './Banner.png'));
+    fs.rmdirSync(path.join(__dirname, '..', './Badges'), { recursive: true });
+    fs.rmdirSync(path.join(__dirname, '..', './.git'), { recursive: true });
 
-delete packageJson.scripts.setup;
-fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(packageJson, null, 4));
+    delete packageJson.scripts.setup;
+    fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(packageJson, null, 4));
 
-fs.unlinkSync(__filename);
+    fs.unlinkSync(__filename);
+} catch (err) {
+    console.error('[Setup]', 'Failed to clean up. Leftover files may exist such as README or LICENSE.');
+}
 
 console.log('[Setup]', 'Install dependencies...');
 
-execSync('pnpm install');
+try {
+    execSync('pnpm install');
+} catch (err) {
+    console.error('[Setup]', 'Failed to install dependencies. Do you have pnpm installed?');
+}
+
+console.log('[Setup]', 'Initializing git...');
+
+try {
+    execSync('git init');
+} catch (err) {
+    console.error('[Setup]', 'Failed to initialize git. Do you have git installed?');
+}
 
 console.log('[Setup]', 'Setup complete.');
