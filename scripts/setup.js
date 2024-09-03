2e5f6778ce26e8c2a9f01568083b52ce6e2ec47a
Zyrenth <work@zyrenth.dev>
2024-09-03 18:48:54 +0200

    script fixes (once again because i'm lazy)

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,18 +7,18 @@ import { execSync } from 'child_process';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
 
 console.log('[Setup]', 'Cleaning up...');
 
-fs.unlinkSync(path.join(__dirname, 'README.md'));
-fs.unlinkSync(path.join(__dirname, 'LICENSE'));
+fs.unlinkSync(path.join(__dirname, '..', 'README.md'));
+fs.unlinkSync(path.join(__dirname, '..', 'LICENSE'));
 
-fs.unlinkSync(path.join(__dirname, './Banner.png'));
-fs.rmdirSync(path.join(__dirname, './Badges'), { recursive: true });
+fs.unlinkSync(path.join(__dirname, '..', './Banner.png'));
+fs.rmdirSync(path.join(__dirname, '..', './Badges'), { recursive: true });
 
 delete packageJson.scripts.setup;
-fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(packageJson, null, 4));
+fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(packageJson, null, 4));
 
 fs.unlinkSync(__filename);
 
